f614fe6093050c989cbbed3721e275e0b566580d
Alex Liu <junhual07@gmail.com>
2022-06-01 01:27:19 -0700

    Changed entry point to account for file restructure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { registerRootComponent } from 'expo';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>Nimloth</Text>
@@ -18,3 +19,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default registerRootComponent(App);
