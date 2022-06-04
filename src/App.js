acb125617938b717b6667143c1e39afbf23d8f34
Alex Liu <junhual07@gmail.com>
2022-06-04 01:27:19 -0700

    Basic input fields for login screen

--- src/App.js
+++ src/App.js
@@ -1,93 +1,52 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import {
-	StyleSheet,
-	Text,
-	TextInput,
-	TouchableOpacity,
-	View,
-	Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Image,
 } from 'react-native';
+import { Input } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { registerRootComponent } from 'expo';
 
 const icon = require('../assets/splash.png');
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	image: {
-		marginBottom: 40,
-	},
-	inputView: {
-		backgroundColor: '#FFC0CB',
-		borderRadius: 30,
-		width: '70%',
-		height: 45,
-		marginBottom: 20,
-		alignItems: 'center',
-	},
-	TextInput: {
-		height: 50,
-		flex: 1,
-		padding: 10,
-		marginLeft: 20,
-	},
-	forgotButton: {
-		height: 30,
-		marginBottom: 30,
-	},
-	loginButton: {
-		width: '80%',
-		borderRadius: 25,
-		height: 50,
-		alignItems: 'center',
-		justifyContent: 'center',
-		marginTop: 40,
-		backgroundColor: '#FF1493',
-	},
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
 
 function App() {
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-	return (
-		<View style={styles.container}>
-			{/* <Image source={icon}/> */}
-			<StatusBar style={{ color: 'auto' }} />
+  return (
+    <View style={styles.container}>
+      <StatusBar style={{ color: 'auto' }} />
 
-			<View style={styles.inputView}>
-				<TextInput
-					style={styles.TextInput}
-					placeholder='Email'
-					placeholderTextColor='#003f5c'
-					onChangeText={(email) => setEmail(email)}
-				/>
-			</View>
-
-			<View style={styles.inputView}>
-				<TextInput
-					style={styles.TextInput}
-					placeholder='Password'
-					placeholderTextColor='#003f5c'
-					secureTextEntry
-					onChangeText={(password) => setPassword(password)}
-				/>
-			</View>
-
-			<TouchableOpacity>
-				<Text style={styles.forgotButton}>Forgot Password</Text>
-			</TouchableOpacity>
-
-			<TouchableOpacity style={styles.loginButton}>
-				<Text style={styles.loginText}>Login</Text>
-			</TouchableOpacity>
-		</View>
-	);
+      <Input
+        label='Email'
+        placeholder='email@address.com'
+        leftIcon={<Icon name='user' size={24} color='black' />}
+        errorStyle={{ color: 'red' }}
+        errorMessage='ERROR'
+      />
+      <Input
+        label='Password'
+        placeholder='Password'
+        leftIcon={<Icon name='user' size={24} color='black' />}
+        errorStyle={{ color: 'red' }}
+        errorMessage='ERROR'
+      />
+    </View>
+  );
 }
 
 export default registerRootComponent(App);
