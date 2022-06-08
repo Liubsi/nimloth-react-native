5a89ad4df68f24d8c8390f6273376c84ce02fd38
Alex Liu <junhual07@gmail.com>
2022-06-07 23:42:51 -0700

    nit

--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native';
 
-export const styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#003f5c',
