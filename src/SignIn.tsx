900e0e58895696122e53fc9e5bb389abbd7167f7
Alex Liu <junhual07@gmail.com>
2022-06-10 19:42:32 -0700

    Setting up screens

---
+++ src/SignIn.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SignInScreen = () => {
+  // TODO
+};
+
+export default SignInScreen;
---
+++ src/SignUp.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SignUpScreen = () => {
+  // TODO
+};
+
+export default SignUpScreen;
