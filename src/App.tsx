833c8733fb7cce8dcf6c3d0ab446e75875fa0458
Alex Liu <junhual07@gmail.com>
2022-06-10 19:13:05 -0700

    Nav

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,31 @@ import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import Login from './Login';
 import Profile from './Profile';
 
+// migrate wrapper to another file
+
 const Stack = createNativeStackNavigator();
 
-function App() {
+const App = () => {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <StatusBar />
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name='Login' component={Login} />
-          <Stack.Screen name='Profile' component={Profile} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <>
+      <StatusBar />
+      <SafeAreaView style={{ flex: 0, backgroundColor: '#9C51B6' }} />
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#5946B2' }}>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+              <Stack.Screen name='Login' component={Login} />
+              <Stack.Screen name='Profile' component={Profile} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </SafeAreaView>
+    </>
   );
-}
+};
 
 export default registerRootComponent(App);
