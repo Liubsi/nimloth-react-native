2d2e57e1b810650b11c1cfe6b3d311ecefc1eabf
Alex Liu <junhual07@gmail.com>
2022-06-10 20:17:51 -0700

    Home folder and file

---
+++ src/Screens/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { CustomText } from '../../components';
+
+const SignUpScreen = () => {
+  return (
+    <CustomText style={{ color: 'white', fontSize: 20 }} textType='regular'>
+      Nimloth
+    </CustomText>
+  );
+};
+
+export default SignUpScreen;
