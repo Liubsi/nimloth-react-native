ad39d5cab23908916d07c79aa13f9105206c847a
Alex Liu <junhual07@gmail.com>
2022-06-12 19:48:34 -0700

    new gradient

--- src/components/LoginBackground.tsx
+++ src/components/LoginBackground.tsx
@@ -7,11 +7,11 @@ type Props = {
 };
 
 const LoginBackground = ({ children }: Props) => {
+  let gradient = ['#9C51B6', '#5946B2'];
+  gradient = ['#2F02B5', '#2B1961'];
+
   return (
-    <LinearGradient
-      colors={['#9C51B6', '#5946B2']}
-      style={styles.linearGradient}
-    >
+    <LinearGradient colors={gradient} style={styles.linearGradient}>
       {children}
     </LinearGradient>
   );
