9749d850cffa66da6cd3d9099454a294cbd2abdf
Alex Liu <junhual07@gmail.com>
2022-06-13 20:13:36 -0700

    Deleted component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 import CustomText from './CustomText';
 import SizedBox from './SizedBox';
 import LoginBackground from './LoginBackground';
-import AvoidKeyboardView from './AvoidKeyboardView';
 
-export { CustomText, SizedBox, LoginBackground, AvoidKeyboardView };
+export { CustomText, SizedBox, LoginBackground };
