a7d393a68e5fefef9f1dc96a0e985e31599c6df0
Alex Liu <junhual07@gmail.com>
2022-06-22 01:15:00 -0700

    test

--- src/Screens/Home/test.ts
+++ src/Screens/Home/test.ts
@@ -1,4 +1,7 @@
-
-export type Action = | { type: 'setEmail'; payload: string }| { type: 'setIsButtonDisabled'; payload: boolean }| { type: 'setPassword'; payload: string }
-  | { type: 'signInSuccess'; payload: string }| { type: 'signInFailed'; payload: string }
+export type Action =
+  | { type: 'setEmail'; payload: string }
+  | { type: 'setIsButtonDisabled'; payload: boolean }
+  | { type: 'setPassword'; payload: string }
+  | { type: 'signInSuccess'; payload: string }
+  | { type: 'signInFailed'; payload: string }
   | { type: 'setIsError'; payload: boolean };
