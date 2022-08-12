a99cf461fa487aaa8f2dce3e9bbf36ae2d1713a2
Alex Liu <junhual07@gmail.com>
2022-08-12 00:05:58 -0700

    initial charts layout

--- src/Screens/Home/MainPages/Charts/index.tsx
+++ src/Screens/Home/MainPages/Charts/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import { View, Dimensions } from 'react-native';
+import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { Text } from '@rneui/themed';
+import Tabs from '@components/Tabs';
+import { SizedBox } from '@components/';
 
 const ChartsScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Text>Charts</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center' }}>
+      <SizedBox height={300} />
+      <Text style={{ textAlign: 'center' }}>Charts</Text>
+      <Tabs tabTitles={['Owned', 'Explore']}>
+        <View style={{ flex: 1, backgroundColor: 'red' }} />
+        <View style={{ flex: 1, backgroundColor: 'blue' }} />
+      </Tabs>
+    </SafeAreaView>
   );
 };
 
