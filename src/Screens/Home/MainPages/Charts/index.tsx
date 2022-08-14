d7cafc11391f8c3300a813548223b550b3d3af34
Alex Liu <junhual07@gmail.com>
2022-08-14 00:23:42 -0700

    add explore and owned coin tab views

--- src/Screens/Home/MainPages/Charts/index.tsx
+++ src/Screens/Home/MainPages/Charts/index.tsx
@@ -1,8 +1,101 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Text } from '@rneui/themed';
+import React, { useState } from 'react';
+import { View, FlatList, TouchableOpacity } from 'react-native';
+import { ListItem } from '@rneui/themed';
 import Tabs from '@components/Tabs';
-import { SizedBox } from '@components/';
+
+type CoinProps = {
+  id: string;
+  coinName: string;
+  dollarAmount: number;
+  ownedAmount: number;
+};
+
+const OwnedCoinsView = () => {
+  const ownedCoinData: CoinProps[] = [
+    { id: '1', coinName: '1', dollarAmount: 1000, ownedAmount: 1 },
+    { id: '2', coinName: '2', dollarAmount: 1000, ownedAmount: 1 },
+    { id: '3', coinName: '3', dollarAmount: 1400, ownedAmount: 1 },
+    { id: '4', coinName: '4', dollarAmount: 1400, ownedAmount: 1 },
+    { id: '5', coinName: '5', dollarAmount: 1400, ownedAmount: 1 },
+    { id: '6', coinName: '6', dollarAmount: 1400, ownedAmount: 1 },
+  ];
+  const [ownedCoins, setOwnedCoins] = useState<CoinProps[]>(ownedCoinData);
+
+  return (
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={ownedCoins}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => {
+              console.log(item);
+            }}
+            style={{ marginBottom: 10 }}
+          >
+            <ListItem key={item.id}>
+              <ListItem.Content>
+                <ListItem.Title
+                  style={{
+                    fontFamily: 'Urbanist',
+                    fontSize: 14,
+                    marginLeft: 20,
+                    marginRight: 20,
+                  }}
+                >
+                  {item.coinName} {item.dollarAmount} {item.ownedAmount}
+                </ListItem.Title>
+              </ListItem.Content>
+            </ListItem>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+};
+
+const ExploreCoinsView = () => {
+  const exploreCoinsData: CoinProps[] = [
+    { id: '6', coinName: '6', dollarAmount: 1000, ownedAmount: 1 },
+    { id: '5', coinName: '5', dollarAmount: 1000, ownedAmount: 1 },
+    { id: '4', coinName: '4', dollarAmount: 1400, ownedAmount: 1 },
+    { id: '3', coinName: '3', dollarAmount: 1400, ownedAmount: 1 },
+    { id: '2', coinName: '2', dollarAmount: 1400, ownedAmount: 1 },
+    { id: '1', coinName: '1', dollarAmount: 1400, ownedAmount: 1 },
+  ];
+  const [exploreCoins, setExploreCoins] =
+    useState<CoinProps[]>(exploreCoinsData);
+
+  return (
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={exploreCoins}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => {
+              console.log(item);
+            }}
+            style={{ marginBottom: 10 }}
+          >
+            <ListItem key={item.id}>
+              <ListItem.Content>
+                <ListItem.Title
+                  style={{
+                    fontFamily: 'Urbanist',
+                    fontSize: 14,
+                    marginLeft: 20,
+                    marginRight: 20,
+                  }}
+                >
+                  {item.coinName} {item.dollarAmount} {item.ownedAmount}
+                </ListItem.Title>
+              </ListItem.Content>
+            </ListItem>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+};
 
 const ChartsScreen = () => {
   return (
@@ -13,8 +106,8 @@ const ChartsScreen = () => {
         <View style={{ flex: 1, backgroundColor: 'green' }} />
       </Tabs>
       <Tabs tabTitles={['Owned', 'Explore']}>
-        <View style={{ flex: 1, backgroundColor: 'red' }} />
-        <View style={{ flex: 1, backgroundColor: 'blue' }} />
+        <OwnedCoinsView />
+        <ExploreCoinsView />
       </Tabs>
     </View>
   );
