453edafd883406631973add7b0e530cfc5708d3d
Marchwarden <charlesgoell@gmail.com>
2022-08-13 21:36:36 -0400

    changes

--- src/components/RingGraph/index.tsx
+++ src/components/RingGraph/index.tsx
@@ -1,101 +1,98 @@
-import React from "react";
-import {View, Text} from 'react-native';
-import {styles} from './styles'
-import Svg, { G, Circle } from "react-native-svg";
-
-
+import React from 'react';
+import { View, Text } from 'react-native';
+import Svg, { G, Circle } from 'react-native-svg';
+import styles from './styles';
 
 const RingGraph = () => {
+  const radius = 65;
+  const borderTest = 10;
+  const circleCircumference = 2 * Math.PI * radius;
+  // Need to fix how data is added here
+  const BTC = 1000;
+  const ETH = 1000;
+  const SOL = 1400;
+  const total = BTC + ETH + SOL;
 
-const radius = 65;
-const borderTest = 10;
-const circleCircumference = 2*Math.PI*radius;
-//Need to fix how data is added here
-const BTC = 1000;
-const ETH = 1000;
-const SOL = 1400;
-const total = BTC+ETH+SOL;
+  const BTCPercentage = (BTC / total) * 100;
+  const ETHPercentage = (ETH / total) * 100;
+  const SOLPercentage = (SOL / total) * 100;
 
-const BTCPercentage = (BTC/total)*100;
-const ETHPercentage = (ETH/total)*100;
-const SOLPercentage = (SOL/total)*100;
-
-const BTCStrokeDashoffset =
-    (circleCircumference - (circleCircumference * BTCPercentage)) / 100;
+  const BTCStrokeDashoffset =
+    (circleCircumference - circleCircumference * BTCPercentage) / 100;
   const ETHStrokeDashoffset =
-    (circleCircumference - (circleCircumference * ETHPercentage)) / 100;
+    (circleCircumference - circleCircumference * ETHPercentage) / 100;
   const SOLStrokeDashoffset =
-    (circleCircumference - (circleCircumference * SOLPercentage)) / 100;
+    (circleCircumference - circleCircumference * SOLPercentage) / 100;
 
-const BTCAngle = (BTC/total)*360;
-const ETHAngle = (ETH/total)*360;
-const SOLAngle = (SOL/total)*360;
+  const BTCAngle = (BTC / total) * 360;
+  const ETHAngle = (ETH / total) * 360;
+  const SOLAngle = (SOL / total) * 360;
 
-return (
-    <View style = {styles.container}>
-        <View style = {styles.graphWrapper}>
-            <Svg height="320" width="320" viewBox="0 0 180 180">
-                <G rotation={-90} originX="90" originY={"90"}>
-                { total === 0 ? (
+  return (
+    <View style={styles.container}>
+      <View style={styles.graphWrapper}>
+        <Svg height='320' width='320' viewBox='0 0 180 180'>
+          <G rotation={-90} originX='90' originY='90'>
+            {total === 0 ? (
               <Circle
-                cx="50%"
-                cy="50%"
+                cx='50%'
+                cy='50%'
                 r={radius}
-                stroke="#F1F6F9"
-                fill="transparent"
+                stroke='#F1F6F9'
+                fill='transparent'
                 strokeWidth={borderTest}
               />
-             ) : (
-               <>
-                 <Circle
-                  cx="50%"
-                  cy="50%"
+            ) : (
+              <>
+                <Circle
+                  cx='50%'
+                  cy='50%'
                   r={radius}
-                  stroke="#FFA41B"
-                  fill="transparent"
+                  stroke='#FFA41B'
+                  fill='transparent'
                   strokeWidth={borderTest}
-                  strokeDasharray={circleCircumference*BTCPercentage}
+                  strokeDasharray={circleCircumference * BTCPercentage}
                   strokeDashoffset={BTCStrokeDashoffset}
                   rotation={0}
-                  originX="90"
-                  originY="90"
-                  strokeLinecap="round"
-                 />
-                 <Circle
-                  cx="50%"
-                  cy="50%"
+                  originX='90'
+                  originY='90'
+                  strokeLinecap='round'
+                />
+                <Circle
+                  cx='50%'
+                  cy='50%'
                   r={radius}
-                  stroke="#1BFF32"
-                  fill="transparent"
+                  stroke='#1BFF32'
+                  fill='transparent'
                   strokeWidth={borderTest}
-                  strokeDasharray={circleCircumference*ETHPercentage}
+                  strokeDasharray={circleCircumference * ETHPercentage}
                   strokeDashoffset={ETHStrokeDashoffset}
                   rotation={BTCAngle}
-                  originX="90"
-                  originY="90"
-                  strokeLinecap="round"
-                 />
-                  <Circle
-                  cx="50%"
-                  cy="50%"
+                  originX='90'
+                  originY='90'
+                  strokeLinecap='round'
+                />
+
+                <Circle
+                  cx='50%'
+                  cy='50%'
                   r={radius}
-                  stroke="#F11BFF"
-                  fill="transparent"
+                  stroke='#F11BFF'
+                  fill='transparent'
                   strokeWidth={borderTest}
-                  strokeDasharray={circleCircumference*SOLPercentage}
+                  strokeDasharray={circleCircumference * SOLPercentage}
                   strokeDashoffset={SOLStrokeDashoffset}
                   rotation={ETHAngle}
-                  originX="90"
-                  originY="90"
-                  strokeLinecap="round"
+                  originX='90'
+                  originY='90'
+                  strokeLinecap='round'
                 />
-                 </>
-                  )
-                  }
-                </G>
-            </Svg>
-        </View>
+              </>
+            )}
+          </G>
+        </Svg>
+      </View>
     </View>
-        );
+  );
 };
 export default RingGraph;
--- src/components/RingGraph/styles.ts
+++ src/components/RingGraph/styles.ts
@@ -1,25 +1,26 @@
 import styled from 'styled-components/native';
-import Svg, {G, Circle} from 'react-native-svg'
+import Svg, { G, Circle } from 'react-native-svg';
 import { StyleSheet, Text } from 'react-native';
 
-  export const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        elevation: 5,
-        top: 220,
-    },
-    graphWrapper:{
-        alignItems: "center",
-        justifyContent: "center",
-        elevation: 5
-    },
-    label: {
-      position: "absolute",
-      textAlign: "center",
-      fontWeight: "700",
-      fontSize: 24,
-      fontFamily: "Urbanist-SemiBold"
-    }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 5,
+    top: 220,
+  },
+  graphWrapper: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 5,
+  },
+  label: {
+    position: 'absolute',
+    textAlign: 'center',
+    fontWeight: '700',
+    fontSize: 24,
+    fontFamily: 'Urbanist-SemiBold',
+  },
 });
+export default styles;
