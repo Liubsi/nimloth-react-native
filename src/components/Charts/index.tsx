ab74c0e3a760c422fcc3deb8fd6b0a4e2336a9a9
Alex Liu <junhual07@gmail.com>
2022-08-19 21:21:32 -0700

    add chart title

--- src/components/Charts/index.tsx
+++ src/components/Charts/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { View } from 'react-native';
 import { line, curveBasis } from 'd3-shape';
 import { scaleLinear, scaleTime } from 'd3-scale';
+import { Text, Icon } from '@rneui/themed';
 import Animated from 'react-native-reanimated';
 import { G, Line, Path, Svg } from 'react-native-svg';
 import addDays from '@common/utils/addDays';
@@ -35,7 +37,6 @@ const FIVE_YEARS = 5 * YEAR;
 export const makeGraph = (data: DataPoint[]) => {
   const yMax = Math.max(...data.map(({ value }) => value));
   const yMin = Math.min(...data.map(({ value }) => value));
-  console.log(yMax, yMin);
   const yAxis = scaleLinear().domain([yMin, yMax]).range([230, 0]);
 
   const xAxis = scaleTime()
@@ -59,6 +60,22 @@ const LineChart = ({ height, width, data, bottomPadding }: LineChartProps) => {
     <Animated.View>
       <Svg width={width} height={height} stroke='black'>
         <G y={-bottomPadding}>
+          <View
+            style={{
+              marginLeft: 30,
+              marginRight: 30,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}
+          >
+            <View style={{ flexDirection: 'row' }}>
+              <Icon type='font-awesome' name='square' size={24} />
+              <Text style={{ marginLeft: 10, fontSize: 18 }}>
+                Premium shit coin
+              </Text>
+            </View>
+            <Text style={{ fontSize: 18 }}>${height}</Text>
+          </View>
           <Line
             x1={0}
             y1={height}
@@ -69,23 +86,24 @@ const LineChart = ({ height, width, data, bottomPadding }: LineChartProps) => {
           />
           <Line
             x1={0}
-            y1={height * 0.6}
+            y1={height * 0.7}
             x2={width}
-            y2={height * 0.6}
+            y2={height * 0.7}
             stroke='#d7d7d7'
             strokeWidth='1'
           />
           <Line
             x1={0}
-            y1={height * 0.2}
+            y1={height * 0.4}
             x2={width}
-            y2={height * 0.2}
+            y2={height * 0.4}
             stroke='#d7d7d7'
             strokeWidth='1'
           />
           <Path d={data[0].curve} strokeWidth='2' />
         </G>
       </Svg>
+      <Text>O</Text>
     </Animated.View>
   );
 };
